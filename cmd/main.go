6021f52cfcb973df8de5ed75fcb5858f309de56e
Kosench <ilyakosenko09@gmail.com>
2024-11-04 18:05:09 +1000

    add storage

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"github.com/go-sql-driver/mysql"
 	"go-backend-api-jwt-mysql/cmd/api"
+	"go-backend-api-jwt-mysql/config"
+	"go-backend-api-jwt-mysql/db"
 	"log"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+	storage, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := api.NewAPIServer(":8080", storage)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
