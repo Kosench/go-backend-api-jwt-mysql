c97ed805eb82ceb6bb934e2e22fbb4bffd4705d6
Kosench <ilyakosenko09@gmail.com>
2024-11-08 21:04:30 +1000

    Implement handleRegister function

--- service/user/routes.go
+++ service/user/routes.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"go-backend-api-jwt-mysql/types"
 	"go-backend-api-jwt-mysql/utils"
@@ -8,10 +9,11 @@ import (
 )
 
 type Handler struct {
+	store types.UserStore
 }
 
-func NewHandler() *Handler {
-	return &Handler{}
+func NewHandler(store types.UserStore) *Handler {
+	return &Handler{store: store}
 }
 
 // RegisterRoutes registers routes for the server API.
@@ -31,5 +33,22 @@ func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
 		utils.WriteError(w, http.StatusBadRequest, err)
 	}
 	//check if the user exists
+	_, err := h.store.GetUserByEmail(payload.Email)
+	if err == nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with email %s already exists"))
+		return
+	}
+
 	//if it doesnt we createthe new user
+	err = h.store.CreateUser(types.User{
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+		Email:     payload.Email,
+		Password:  payload.Password,
+	})
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+	utils.WriteJSON(w, http.StatusCreated, nil)
 }
