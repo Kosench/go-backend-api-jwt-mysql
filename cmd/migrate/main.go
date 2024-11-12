47ab44f389205c66184b84157ffe8f3e3ca8be03
Kosench <ilyakosenko09@gmail.com>
2024-11-12 16:38:11 +1000

    Add migration logic for MySQL with go-migrate

---
+++ cmd/migrate/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"errors"
+	mysqlCfg "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"go-backend-api-jwt-mysql/config"
+	"go-backend-api-jwt-mysql/db"
+	"log"
+	"os"
+)
+
+func main() {
+	storage, err := db.NewMySQLStorage(mysqlCfg.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	driver, err := mysql.WithInstance(storage, &mysql.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	m, err := migrate.NewWithDatabaseInstance(
+		"file/://cmd/migrate/migrations",
+		"mysql",
+		driver,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := os.Args[(len(os.Args) - 1)]
+	if cmd == "up" {
+		if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+			log.Fatal(err)
+		}
+	}
+	if cmd == "down" {
+		if err := m.Down(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+			log.Fatal(err)
+		}
+	}
+}
