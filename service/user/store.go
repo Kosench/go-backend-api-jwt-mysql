6db03bdf2a095ca2bb063660cf6894884db6b2bc
Kosench <ilyakosenko09@gmail.com>
2024-11-15 16:42:09 +1000

    Implement GetUserByID and CreateUser functions

--- service/user/store.go
+++ service/user/store.go
@@ -15,13 +15,13 @@ func NewStore(db *sql.DB) *Store {
 }
 
 func (s *Store) GetUserByEmail(email string) (*types.User, error) {
-	rows, err := s.db.Query("SELECT * FROM users WHERE email = ?")
+	rows, err := s.db.Query("SELECT * FROM ecom.users WHERE email = ?", email)
 	if err != nil {
 		return nil, err
 	}
 	u := new(types.User)
 	for rows.Next() {
-		u, err = scanRowIntoUser(rows)
+		u, err = scanRowsIntoUser(rows)
 		if err != nil {
 			return nil, err
 		}
@@ -33,7 +33,37 @@ func (s *Store) GetUserByEmail(email string) (*types.User, error) {
 	return u, err
 }
 
-func scanRowIntoUser(rows *sql.Rows) (*types.User, error) {
+func (s *Store) GetUserByID(id int) (*types.User, error) {
+	rows, err := s.db.Query("SELECT * FROM ecom.users WHERE id = ?", id)
+	if err != nil {
+		return nil, err
+	}
+
+	u := new(types.User)
+	for rows.Next() {
+		u, err = scanRowsIntoUser(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if u.ID == 0 {
+		return nil, fmt.Errorf("user not found")
+	}
+
+	return u, nil
+}
+
+func (s *Store) CreateUser(user types.User) error {
+	_, err := s.db.Exec("INSERT INTO ecom.users (firstName, lastName, email, password) VALUES (?,?,?,?)",
+		user.FirstName, user.LastName, user.Email, user.Password)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func scanRowsIntoUser(rows *sql.Rows) (*types.User, error) {
 	user := new(types.User)
 	err := rows.Scan(
 		&user.ID,
@@ -47,12 +77,4 @@ func scanRowIntoUser(rows *sql.Rows) (*types.User, error) {
 		return nil, err
 	}
 	return user, err
-}
-
-func (s *Store) GetUserByID(id int) (*types.User, error) {
-	return nil, nil
-}
-
-func (s *Store) CreateUser(user types.User) error {
-	return nil
 }
