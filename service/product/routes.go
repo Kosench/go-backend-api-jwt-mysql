64f677a235aab786535b9ada4a43e3980ab0f4ab
Kosench <ilyakosenko09@gmail.com>
2024-11-27 20:51:02 +1000

    add methods handleGetProduct, HandleGetProducts, CreateProduct

--- service/product/routes.go
+++ service/product/routes.go
@@ -1,10 +1,13 @@
 package product
 
 import (
+	"fmt"
+	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
 	"go-backend-api-jwt-mysql/types"
 	"go-backend-api-jwt-mysql/utils"
 	"net/http"
+	"strconv"
 )
 
 type Handler struct {
@@ -17,13 +20,71 @@ func NewHandler(store types.ProductStore) *Handler {
 
 func (h *Handler) RegisterStore(router *mux.Router) {
 	router.HandleFunc("/products", h.handleCreateProduct).Methods(http.MethodGet)
+	router.HandleFunc("/products", h.handleCreateProduct).Methods(http.MethodPost)
+
 }
 
-func (h Handler) handleCreateProduct(w http.ResponseWriter, r *http.Request) {
-	ps, err := h.store.GetProduct()
+func (h *Handler) handleCreateProduct(w http.ResponseWriter, r *http.Request) {
+	ps, err := h.store.GetProducts()
 	if err != nil {
 		utils.WriteError(w, http.StatusInternalServerError, err)
 		return
 	}
 	utils.WriteJSON(w, http.StatusOK, ps)
+}
+
+func (h *Handler) HandleGetProducts(w http.ResponseWriter, r *http.Request) {
+	products, err := h.store.GetProducts()
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, products)
+}
+
+func (h Handler) handleGetProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	str, ok := vars["product[id]"]
+	if !ok {
+		utils.WriteError(w, http.StatusInternalServerError, fmt.Errorf("missing product ID"))
+		return
+	}
+
+	productID, err := strconv.Atoi(str)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid product ID"))
+		return
+	}
+
+	product, err := h.store.GetProductByID(productID)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, product)
+}
+
+func (h Handler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product types.CreateProductPayload
+	if err := utils.ParseJSON(r, &product); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err := utils.Validate.Struct(product); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload: %v", errors))
+		return
+	}
+
+	err := h.store.CreateProduct(product)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, product)
+
 }
