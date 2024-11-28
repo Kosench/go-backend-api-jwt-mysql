d6aed97c7e7f7e57a4a05d1fb061bdeaae79ed10
Kosench <ilyakosenko09@gmail.com>
2024-11-28 15:07:03 +1000

    add interface OrderStore, structs CartCheckoutIyem, Order, OrderItem

--- types/types.go
+++ types/types.go
@@ -18,6 +18,11 @@ type ProductStore interface {
 	UpdateProduct(Product) error
 }
 
+type OrderStore interface {
+	CreateOrder(Order) (int, error)
+	CreateOrderItem(OrderItem) error
+}
+
 type Product struct {
 	ID          int       `json:"id"`
 	Name        string    `json:"name"`
@@ -26,6 +31,29 @@ type Product struct {
 	Price       float64   `json:"price"`
 	Quantity    int       `json:"quantity"`
 	CreatedAt   time.Time `json:"createAt"`
+}
+
+type CartCheckoutIyem struct {
+	ProductID int `json:"productID"`
+	Quantity  int `json:"quantity"`
+}
+
+type Order struct {
+	ID        int       `json:"id"`
+	UserID    int       `json:"userID"`
+	Total     float64   `json:"total"`
+	Status    string    `json:"status"`
+	Address   string    `json:"address"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type OrderItem struct {
+	ID        int       `json:"id"`
+	OrderID   int       `json:"orderID"`
+	ProductID int       `json:"productID"`
+	Quantity  int       `json:"quantity"`
+	Price     float64   `json:"price"`
+	CreatedAt time.Time `json:"createdAt"`
 }
 
 type CreateProductPayload struct {
