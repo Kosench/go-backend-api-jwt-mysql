fe9416dacf4e3f057d20e131ca133405f138499a
Kosench <ilyakosenko09@gmail.com>
2024-12-06 20:54:52 +1000

    Add getTokenFromRequest function for authorization token retrieval

--- service/auth/jwt.go
+++ service/auth/jwt.go
@@ -3,6 +3,8 @@ package auth
 import (
 	"github.com/golang-jwt/jwt/v5"
 	"go-backend-api-jwt-mysql/config"
+	"go-backend-api-jwt-mysql/types"
+	"net/http"
 	"strconv"
 	"time"
 )
@@ -20,4 +22,23 @@ func CreateJWT(secret []byte, userID int) (string, error) {
 		return "", err
 	}
 	return tokenString, nil
+}
+
+func WithJWTAuth(handleFunc http.HandlerFunc, store types.UserStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// get the token from the user request
+		// validate the JWT
+		// if is we need to fetch the userID from DB (from the token)
+		//set context user_ID to the user
+	}
+}
+
+func getTokenFromRequest(r *http.Request) string {
+	tokenAuth := r.Header.Get("Authorization")
+
+	if tokenAuth != "" {
+		return tokenAuth
+	}
+
+	return ""
 }
