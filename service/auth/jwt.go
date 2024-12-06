c07bd269758c6fd9629a630b1bebfc690176a0c3
Kosench <ilyakosenko09@gmail.com>
2024-12-06 21:17:19 +1000

    Implement JWT validation function

--- service/auth/jwt.go
+++ service/auth/jwt.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"fmt"
 	"github.com/golang-jwt/jwt/v5"
 	"go-backend-api-jwt-mysql/config"
 	"go-backend-api-jwt-mysql/types"
@@ -27,6 +28,8 @@ func CreateJWT(secret []byte, userID int) (string, error) {
 func WithJWTAuth(handleFunc http.HandlerFunc, store types.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// get the token from the user request
+		tokenString := getTokenFromRequest(r)
+
 		// validate the JWT
 		// if is we need to fetch the userID from DB (from the token)
 		//set context user_ID to the user
@@ -41,4 +44,14 @@ func getTokenFromRequest(r *http.Request) string {
 	}
 
 	return ""
+}
+
+func validateJWT(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(config.Envs.JWTSecret), nil
+	})
 }
